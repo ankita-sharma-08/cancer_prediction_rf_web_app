340cc2f8d2acaf04b3adb3f79df1bb91615369e3
Ankita Sharma <ankitasharma4147@gmail.com>
2025-04-24 21:31:48 +0530

    Update App.py

--- App.py
+++ App.py
@@ -7,7 +7,7 @@ with open('random_forest_classifier.pkl', 'rb') as f:
     model = pickle.load(f)
 
 # Streamlit UI
-st.title("Cancer Prediction Web App")
+st.title("Cancer Prediction Web App By Ankita")
 st.write("🔍 This app uses a Random Forest Classifier to predict type of cancer.")
 
 # Collect user input
